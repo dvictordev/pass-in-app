1107ecaf58e20c3790edc360a38e3326571435e5
victor duarte <victor.d.dev@gmail.com>
2024-04-03 14:41:58 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+/* 
+sempre que for uma nova pagina exportar função como default
+*/
+export default function Home() {
+  return (
+    <View>
+      <Text> Olá mundo! </Text>
+    </View>
+  );
+}
